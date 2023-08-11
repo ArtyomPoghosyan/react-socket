8a4b6155156ee8029d3e9c7209ce7d1c25fe2d9b
Tyom <artyom.mikayelyan@padcllc.com>
2023-08-11 11:52:59 +0400

    added type for apiKey

--- src/socket.tsx
+++ src/socket.tsx
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const apiKey = import.meta.env.VITE_SOME_KEY;
+const apiKey:string = import.meta.env.VITE_URL;
 
-export const socket = io(apiKey);
+export const socket = io(apiKey)
