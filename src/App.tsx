b55aa10ea61c796162bf84fc81c740e3168bce7f
Tyom <artyom.mikayelyan@padcllc.com>
2023-08-11 15:58:06 +0400

    socket and i18n translation

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { useEffect, useState } from 'react';
 import { socket } from './socket';
 
 import './App.css'
-import { Chat } from './pages/chat';
+// import { Chat } from './pages/chat';
+import { Translation } from './pages/translation';
 
 function App() {
   return (
     <div className="App">
-      <Chat />
+      {/* <Chat /> */}
+      <Translation/>
     </div>
   );
 }
